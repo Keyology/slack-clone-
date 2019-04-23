cee64b8cce724699de47c405ecad06cd764259c0
Keyology <keonimurray45@gmail.com>
2019-04-23 11:33:44 -0700

    establish socket.io connection

--- server.js
+++ server.js
@@ -4,15 +4,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 const server = require("http").Server(app);
 
+//Socket.io
+const io = require("socket.io")(server);
+io.on("connection", socket => {
+  console.log("🔌 New user connected! 🔌");
+});
+
 //Express View Engine for Handlebarss
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.handlebars");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+// app.listen(port, () => {
+//   console.log(`listening on port ${port}`);
+// });
